8e782c8d34d58e82c46c9379820d591975633282
dontae1 <brandonium21@gmail.com>
2015-02-06 15:57:59 -0600

    changed port

--- runserver.py
+++ runserver.py
@@ -9,4 +9,4 @@ init_app(app, db)
 
 # Start a development web server if executed from the command line
 if __name__ == "__main__":
-    app.run(host = '192.168.1.122', port=5009, debug=True)
+    app.run()
